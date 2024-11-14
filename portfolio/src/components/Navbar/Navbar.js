0aaa9b9493c6456b6ec4d82841702a841efc7d90
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2024-11-14 12:51:45 -0800

    added navbar template

---
+++ portfolio/src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/programmer.png'
+import contactMe from '../../assets/contact.png'
+import {Link} from 'react-scroll'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <img src={logo} alt='Logo' className='logo'/>
+        <div className='nav-menu'>
+            <Link className='nav-menu-item'>Home</Link>
+            <Link className='nav-menu-item'>About me</Link>
+            <Link className='nav-menu-item'>Projects</Link>
+            <Link className='nav-menu-item'>Experience</Link>
+        </div>
+        <button className='contact-button'>
+            <img src={contactMe} alt='' className='contact-button-img' />
+            Contact Me
+        </button>
+    </nav>
+  )
+}
+
+export default Navbar
