173e43616482bbf2c91de6c9c91d5ed3d3f7dde1
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2024-11-19 02:57:11 -0800

    added theme switch effect for about me component

--- portfolio/src/components/AboutMe/aboutme.js
+++ portfolio/src/components/AboutMe/aboutme.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import './aboutme.css'
+import { useTheme } from '../../contexts/ThemeContext';
 
 const Aboutme = () => {
+
+  const { isDarkMode } = useTheme();
+
   return (
       <div className='skills'>
-          <span className='skillsTitle'>What I work on</span>
-          <span className='skillsDesc'>I specialize in innovative solutions across machine learning, software engineering, and full-stack development. With expertise in predictive modeling, natural language processing, and computer vision, I build scalable applications with robust backends, responsive frontends, and seamless API integrations. My strong foundation in programming, data analysis, and cloud technologies enables me to craft efficient, user-centric, and high-performing solutions.</span>
+          <span className={`skillsTitle ${isDarkMode}`}>What I work on</span>
+          <span className={`skillsDesc ${isDarkMode}`}>I specialize in innovative solutions across machine learning, software engineering, and full-stack development. With expertise in predictive modeling, natural language processing, and computer vision, I build scalable applications with robust backends, responsive frontends, and seamless API integrations. My strong foundation in programming, data analysis, and cloud technologies enables me to craft efficient, user-centric, and high-performing solutions.</span>
           <div className='skillsSection'>
-            <div className='skillsItem'>
+            <div className={`skillsItem ${isDarkMode}`}>
               <div className='skillItemText'>
                 <h3>Full Stack Development</h3>
                 <p>React.js, Vue.js, Spring Boot, Flask, Node.js, RESTful APIs, Docker, Maven, CI/CD, MySQL, PostgreSQL, Hibernate ORM</p>
               </div>
             </div>
-            <div className='skillsItem'>
+            <div className={`skillsItem ${isDarkMode}`}>
               <div className='skillItemText'>
                 <h3>Machine Learning and AI</h3>
                 <p>PyTorch, scikit-learn, TensorFlow, LSTM, BERT, YOLOv4, SVM, Matplotlib, Seaborn, NumPy, Pandas</p>
               </div>
             </div>
-            <div className='skillsItem'>
+            <div className={`skillsItem ${isDarkMode}`}>
               <div className='skillItemText'>
                 <h3>Programming and Other Tools/Frameworks</h3>
                 <p>Python, Java, JavaScript, TypeScript, C, C++, Bash Scripting, Linux, Azure AD, JIRA, AWS, Git, Beautiful Soup</p>
