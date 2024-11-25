c878775d2caf9761af228e53b901741f70b6502d
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2024-11-25 01:14:00 -0800

    adding EmailJS facility in contact page

--- src/components/ContactMe/contactme.js
+++ src/components/ContactMe/contactme.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './contactme.css'
-import { useTheme } from '../../contexts/ThemeContext';
+import { useTheme } from '../../contexts/ThemeContext'
+import emailjs from '@emailjs/browser';
 
 const ContactMe = () => {
 
   const { isDarkMode } = useTheme();
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [statusMessage, setStatusMessage] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.send(
+        'service_x2bw2ms',
+        'template_ph96dlu',
+        {
+            name: formData.name,
+            email: formData.email,
+            message: formData.message,
+        },
+        'V1rs-u61pvBlhLWio'
+    )
+    .then(
+        (response) => {
+            setStatusMessage("Email sent successfully");
+            setFormData({ name: '', email: '', message: '' });
+            setTimeout(() => setStatusMessage(''), 5000);
+        },
+        (err) => {
+            setStatusMessage('Failed to send email. Please try again later.');
+            setTimeout(() => setStatusMessage(''), 5000);
+        }
+    );
+  };
 
   return (
     <div className="contact-me-page">
@@ -12,18 +50,15 @@ const ContactMe = () => {
       <p className={`contact-me-subheading ${isDarkMode}`}>
         Let's discuss any work opportunities.
       </p>
-      <form className="contact-me-form">
-        <input type="text" placeholder="Your name" className="form-input" />
-        <input type="email" placeholder="Your Email" className="form-input" />
-        <textarea
-          placeholder="Your Message"
-          className="form-textarea"
-          rows="5"
-        ></textarea>
+      <form className="contact-me-form" onSubmit={handleSubmit}>
+        <input type="text" name="name" value={formData.name} placeholder="Your name" className="form-input" onChange={handleChange} required />
+        <input type="email" name="email" value={formData.email} placeholder="Your Email" className="form-input" onChange={handleChange} required/>
+        <textarea name="message" placeholder="Your Message" className="form-textarea" rows="5" value={formData.message} onChange={handleChange} required/>
         <button type="submit" className={`form-submit-button ${isDarkMode}`}>
           Submit
         </button>
       </form>
+        {statusMessage && <p className="status-message">{statusMessage}</p>}
     </div>
   )
 }
