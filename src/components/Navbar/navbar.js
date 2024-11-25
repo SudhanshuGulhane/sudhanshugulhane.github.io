0606293f1bac048b909a77aa49c8a3125be9da14
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2024-11-25 14:04:26 -0800

    added responsive menu in the navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -8,8 +8,14 @@ const Navbar = () => {
   const { isDarkMode, toggleTheme } = useTheme();
   const [isSticky, setIsSticky] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 620);
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 620);
+    };
+
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       const experienceSection = document.querySelector('.experience-section h2');
@@ -39,26 +45,46 @@ const Navbar = () => {
       setPrevScrollPos(currentScrollPos);
     };
 
+    window.addEventListener('resize', handleResize);
     window.addEventListener('scroll', handleScroll);
     return () => {
+      window.removeEventListener('resize', handleResize);
       window.removeEventListener('scroll', handleScroll);
     };
   }, [prevScrollPos]);
+
+  const handleLinkClick = () => {
+    if (window.innerWidth <= 620) {
+      toggleMenu();
+    }
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className={`navbar ${isSticky ? 'sticky' : 'not-sticky'}`}>
         <button className={`toggle-button ${isDarkMode}`} onClick={toggleTheme}>
           <i className={isDarkMode==="dark" ? 'fas fa-sun' : 'fas fa-moon'}></i>
         </button>
-        <div className='nav-menu'>
-            <Link to="/" className={`nav-menu-item ${isDarkMode}`}>Home</Link>
-            <Link to="/aboutme" className={`nav-menu-item ${isDarkMode}`}>About me</Link>
-            <Link to="/projects" className={`nav-menu-item ${isDarkMode}`}>Projects</Link>
-            <Link to="/experience"className={`nav-menu-item ${isDarkMode}`}>Experience</Link>
+        <div className={`hamburger-menu ${isMenuOpen ? 'open': ''}`} onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
         </div>
-        <Link to="/contact" className={`contact-button ${isDarkMode}`}>
-          Contact Me
-        </Link>
+        <div className={`nav-menu ${isMenuOpen ? 'show': ''}`}>
+            <Link to="/" className={`nav-menu-item ${isDarkMode}`} onClick={handleLinkClick}>Home</Link>
+            <Link to="/aboutme" className={`nav-menu-item ${isDarkMode}`} onClick={handleLinkClick}>About me</Link>
+            <Link to="/projects" className={`nav-menu-item ${isDarkMode}`} onClick={handleLinkClick}>Projects</Link>
+            <Link to="/experience"className={`nav-menu-item ${isDarkMode}`} onClick={handleLinkClick}>Experience</Link>
+            {isSmallScreen && (
+              <Link to="/contact" className={`nav-menu-item ${isDarkMode}`} onClick={handleLinkClick}>Contact Me</Link>
+            )}
+        </div>
+        {!isSmallScreen &&(
+          <Link to="/contact" className={`contact-button ${isDarkMode}`} onClick={handleLinkClick}>Contact Me</Link>
+        )}
     </nav>
   )
 }
