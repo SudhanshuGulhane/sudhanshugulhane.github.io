a82667cbe85cd55723224433fa7bcec8a4233b18
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2025-02-16 12:13:42 -0800

    subtle changes for about me section

--- src/components/AboutMe/aboutme.js
+++ src/components/AboutMe/aboutme.js
@@ -24,19 +24,19 @@ const Aboutme = () => {
             <div className={`skillsItem ${isDarkMode}`}>
               <div className='skillItemText'>
                 <h3>Full Stack Development</h3>
-                <p>React.js, Vue.js, Spring Boot, Flask, Node.js, RESTful APIs, Docker, Maven, CI/CD, MySQL, PostgreSQL, Hibernate ORM</p>
+                <p>React.js, Vue.js, Spring Boot, Flask, Node.js, RESTful APIs, Docker, Maven, CI/CD, MySQL, PostgreSQL, Hibernate ORM, AWS(EC2, Lambda), Azure, Git (GitHub, Bitbucket), Jira, Apache Hadoop, Hive, Spark, NoSQL</p>
               </div>
             </div>
             <div className={`skillsItem ${isDarkMode}`}>
               <div className='skillItemText'>
                 <h3>Machine Learning and AI</h3>
-                <p>PyTorch, scikit-learn, TensorFlow, LSTM, BERT, YOLOv4, SVM, Matplotlib, Seaborn, NumPy, Pandas</p>
+                <p>PyTorch, scikit-learn, TensorFlow,  Hugging Face, LSTM, OpenCV, NLP with BERT, YOLOv4, SVM, Matplotlib, Seaborn, NumPy, Pandas</p>
               </div>
             </div>
             <div className={`skillsItem ${isDarkMode}`}>
               <div className='skillItemText'>
-                <h3>Programming and Other Tools/Frameworks</h3>
-                <p>Python, Java, JavaScript, TypeScript, C, C++, Bash Scripting, Linux, Azure AD, JIRA, AWS, Git, Beautiful Soup</p>
+                <h3>Programming Languages and OS</h3>
+                <p>C, C++, Python, Java, JavaScript, Linux, Windows</p>
               </div>
             </div>
             
